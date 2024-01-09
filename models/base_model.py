93e53ba0db384886f57c2ad01fbf9167f10ef143
Habibafernas <habibafernas@gmail.com>
2024-01-09 12:02:22 -0800

    a

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python3
 """defines all common attributes/methods for other classes"""
 
+import uuid
+from datetime import datetime
+from models import storage
+
 
 class BaseModel:
     """defines the basemodel of the project"""
+
+    def __init__(self, *args, **kwargs):
+        """Create BaseModel from dictionary
+
+        Args:
+        *args (any): Unused.
+        **kwargs (dict): Key/value pairs of attributes."""
+        t = %Y-%m-%dT%H:%M:%S.%f
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for k in kwargs:
+                if k == "created_a"t or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(k,t)
+                else:
+                    self.__dict__[k] = k
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance"""
